736c64e988a2d3ff3734696aa9645e9ea3a1a154
Dinh-Van Colomban <van.colomban@gmail.com>
2025-03-02 01:50:34 +0100

    chore(demo): update stepper demo

--- demo/components/DemoStepper.svelte
+++ demo/components/DemoStepper.svelte
@@ -1,49 +1,65 @@
 <script lang="ts">
-  import NeoSelect from '../../src/lib/inputs/NeoSelect.svelte';
+  import { tick } from 'svelte';
 
+  import type { NeoFormContext } from '~/inputs/common/neo-form-context.svelte';
+  import type { NeoFormHTMLElement } from '~/inputs/common/neo-form.model';
   import type { NeoStepperEvent, NeoStepperPlacements, NeoStepperProps, NeoStepperStep } from '~/stepper/neo-stepper.model';
 
-  import { displayValue } from '~';
   import NeoButton from '~/buttons/NeoButton.svelte';
 
   import NeoButtonGroup from '~/buttons/NeoButtonGroup.svelte';
 
+  import IconAccount from '~/icons/IconAccount.svelte';
   import NeoNumberStep from '~/inputs/NeoNumberStep.svelte';
 
+  import NeoPassword from '~/inputs/NeoPassword.svelte';
+  import NeoPin from '~/inputs/NeoPin.svelte';
+  import NeoSelect from '~/inputs/NeoSelect.svelte';
+  import NeoTextarea from '~/inputs/NeoTextarea.svelte';
+  import NeoForm from '~/inputs/common/NeoForm.svelte';
+  import NeoInput from '~/inputs/common/NeoInput.svelte';
+  import { displayValue } from '~/inputs/neo-select.model';
   import NeoStepper from '~/stepper/NeoStepper.svelte';
   import { NeoStepperPlacement } from '~/stepper/neo-stepper.model';
   import { DefaultShadowShallowElevation } from '~/utils/shadow.utils';
 
+  let context = $state<NeoFormContext>();
+  let ref = $state<NeoFormHTMLElement>();
+  const step1Value = $state({ username: '', password: '' });
+
   const steps: NeoStepperStep[] = [
     {
-      previous: true,
+      markProps: {
+        label: 'Account',
+        ariaLabel: 'Step 1',
+        title: 'Step 1',
+        icon: account,
+      },
+      get next() {
+        return !!context?.valid;
+      },
+      render: step1,
+      value: step1Value,
+      onStep: async (previous, current) => {
+        if (!previous || previous < current) return;
+        await tick();
+        ref?.validate();
+        console.info('Step 1: revalidate', context?.fields);
+      },
     },
     {
-      next: true,
-      previous: true,
+      markProps: {
+        label: 'Address',
+        ariaLabel: 'Step 2',
+        title: 'Step 2',
+      },
+      render: step2,
     },
     {
-      next: true,
-      previous: false,
-    },
-    4,
-    {
-      next: 2,
-      cancel: false,
-      previous: 1,
-    },
-    6,
-    7,
-    8,
-    {
-      disabled: true,
-    },
-    {
-      next: true,
-      nextProps: {
-        label: 'Finish',
-        ariaLabel: 'Finish stepper',
-        title: 'Finish stepper',
+      markProps: {
+        label: 'Files',
+        ariaLabel: 'Step 3',
+        title: 'Step 3',
       },
     },
   ];
@@ -69,11 +85,14 @@
     loop: false,
 
     elevation: DefaultShadowShallowElevation,
+    borderless: false,
 
     vertical: false,
     placement: NeoStepperPlacement.Start,
 
-    onStep: (event: NeoStepperEvent) => console.info('Stepper step:', event),
+    onStep: (event: NeoStepperEvent) => {
+      console.info('Stepper step:', event);
+    },
   });
 </script>
 
@@ -85,6 +104,7 @@
     <NeoButton toggle bind:checked={options.controls}>Controls</NeoButton>
     <NeoButton toggle bind:checked={options.loop}>Loop</NeoButton>
     <NeoButton toggle bind:checked={options.vertical}>Vertical</NeoButton>
+    <NeoButton toggle bind:checked={options.borderless}>Borderless</NeoButton>
   </NeoButtonGroup>
 
   <NeoNumberStep
@@ -116,14 +136,72 @@
   />
 </div>
 
+{#snippet account()}
+  <IconAccount />
+{/snippet}
+
+{#snippet step1()}
+  <div class="form">
+    <NeoInput
+      bind:value={step1Value.username}
+      name="username"
+      required
+      label="Username"
+      autocomplete="username"
+      flex="1 0 100%"
+      validation="error"
+      elevation={options.elevation}
+    />
+    <NeoPassword
+      bind:value={step1Value.password}
+      name="password"
+      required
+      label="Password"
+      autocomplete="new-password"
+      flex="1 0 100%"
+      validation="error"
+      elevation={options.elevation}
+    />
+  </div>
+{/snippet}
+
+{#snippet step2()}
+  <div class="form">
+    <NeoTextarea name="address" label="Address" autocomplete="street-address" resize="none" rows="1" flex="1 0 100%" />
+    <NeoInput name="city" label="City" autocomplete="address-level2" flex="1 0 100%" />
+    <NeoSelect
+      name="country"
+      label="Country"
+      placeholder="Country"
+      autocomplete="country"
+      flex="1 0 100%"
+      options={[
+        { label: 'United States', value: 'US' },
+        { label: 'Canada', value: 'CA' },
+        { label: 'United Kingdom', value: 'UK' },
+        { label: 'France', value: 'FR' },
+        { label: 'Germany', value: 'DE' },
+        { label: 'Italy', value: 'IT' },
+        { label: 'Spain', value: 'ES' },
+        { label: 'Japan', value: 'JP' },
+        { label: 'China', value: 'CN' },
+        { label: 'Australia', value: 'AU' },
+      ]}
+    />
+    <NeoPin name="zip" label="Zip Code" count="5" autocomplete="postal-code" />
+  </div>
+{/snippet}
+
 <div class="row">
   <div class="column content">
     <span class="label">Default</span>
-    <NeoStepper {steps} {...options}>
-      {#snippet children({ active })}
-        {active + 1}
-      {/snippet}
-    </NeoStepper>
+    <NeoForm bind:context bind:ref>
+      <NeoStepper {steps} {...options}>
+        {#snippet children()}
+          <NeoTextarea name="address" label="Address" autocomplete="street-address" resize="none" width="100%" />
+        {/snippet}
+      </NeoStepper>
+    </NeoForm>
   </div>
 </div>
 
@@ -135,14 +213,19 @@
     white-space: pre-line;
   }
 
+  .form {
+    display: flex;
+    flex-direction: column;
+  }
+
   .column {
     @include flex.column($center: true, $gap: var(--neo-gap-lg));
 
-    width: max(80vw, 30rem);
-
-    :global(.neo-stepper-content-step) {
-      width: min(80vw, 40rem);
-      height: min(80vh, 40rem);
+    &.content {
+      :global(.neo-stepper-content-step) {
+        min-width: min(80vw, 30rem);
+        min-height: min(80vh, 30rem);
+      }
     }
   }
 
