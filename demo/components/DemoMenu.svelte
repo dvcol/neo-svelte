8e212a81dafe9bbf3d98187eb6f65a910f1aeb3b
Dinh-Van Colomban <van.colomban@gmail.com>
2025-04-02 16:18:05 +0200

    chore(demo): rework router config

---
+++ demo/components/DemoMenu.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import { colorOptions } from '../utils/color.utils';
+
+  import NeoButton from '~/buttons/NeoButton.svelte';
+  import NeoButtonGroup from '~/buttons/NeoButtonGroup.svelte';
+  import NeoSelect from '~/inputs/NeoSelect.svelte';
+  import { displayValue } from '~/inputs/neo-select.model';
+
+  const options = $state({
+    color: '',
+    close: false,
+  });
+</script>
+
+<div class="row">
+  <NeoButtonGroup text rounded>
+    <NeoButton toggle bind:checked={options.close}>Close</NeoButton>
+  </NeoButtonGroup>
+</div>
+
+<div class="row">
+  <NeoSelect
+    label="Color"
+    placeholder="Select color"
+    placement="left"
+    floating={false}
+    color={options.color}
+    display={displayValue}
+    size="10"
+    bind:value={options.color}
+    containerProps={{ style: 'margin-left: 4rem' }}
+    options={colorOptions}
+    openOnFocus
+    rounded
+    glass
+  />
+</div>
+
+<section>
+  <div class="row">
+    <span class="label">Menu</span>
+  </div>
+</section>
+
+<style lang="scss">
+  @use 'src/lib/styles/common/flex' as flex;
+
+  section {
+    flex: 1 1 100%;
+    align-content: center;
+  }
+
+  .label {
+    max-width: 80vw;
+    white-space: pre-line;
+    word-break: break-all;
+  }
+
+  .row {
+    @include flex.row($center: true, $gap: var(--neo-gap-xl), $flex: 0 1 auto);
+
+    margin: 4rem 0;
+  }
+
+  @media (width < 1200px) {
+    .row {
+      margin: 2rem 0;
+    }
+  }
+</style>
--- demo/router/routes.ts
+++ demo/router/routes.ts
@@ -1,7 +1,6 @@
 import type { RouterOptions } from '@dvcol/svelte-simple-router/models';
 
 export const Route = {
-  Any: 'any' as const,
   Buttons: 'buttons' as const,
   ButtonGroups: 'button-groups' as const,
   Pill: 'pill' as const,
@@ -19,128 +18,28 @@ export const Route = {
   Cursor: 'cursor' as const,
   Tooltips: 'tooltips' as const,
   Dialog: 'dialog' as const,
+  Menu: 'menu' as const,
+  Any: 'any' as const,
 } as const;
 
 export type Routes = (typeof Route)[keyof typeof Route];
 
-export const Path: Record<keyof typeof Route, string> = {
-  Buttons: '/buttons' as const,
-  ButtonGroups: '/buttons/groups' as const,
-  Pill: '/pill' as const,
-  Tabs: '/tabs' as const,
-  TabsPanels: '/tabs/panels' as const,
-  Dividers: '/dividers' as const,
-  Cards: '/cards' as const,
-  Text: '/text' as const,
-  Skeleton: '/skeleton' as const,
-  Inputs: '/inputs' as const,
-  Lists: '/lists' as const,
-  Progress: '/progress' as const,
-  Collapse: '/collapse' as const,
-  Stepper: '/stepper' as const,
-  Cursor: '/cursor' as const,
-  Tooltips: '/tooltips' as const,
-  Dialog: '/dialog' as const,
-  Any: '*' as const,
-} as const;
+type RouteKeys = keyof typeof Route;
+export const Path = {} as Record<RouteKeys, string>;
+export const PathMap = {} as Record<Routes, string>;
+
+(Object.entries(Route) as [RouteKeys, Routes][]).forEach(([k, v]) => {
+  Path[k] = `/${v}`;
+  PathMap[v] = `/${v}`;
+});
 
 export const routes = Object.values(Route).filter(key => key !== Route.Any);
 
 export const options: RouterOptions<Routes> = {
   hash: true,
   listen: 'navigation',
-  routes: [
-    {
-      name: Route.Buttons,
-      path: Path.Buttons,
-      component: () => import('../components/DemoButtons.svelte'),
-    },
-    {
-      name: Route.ButtonGroups,
-      path: Path.ButtonGroups,
-      component: () => import('../components/DemoButtonGroups.svelte'),
-    },
-    {
-      name: Route.Pill,
-      path: Path.Pill,
-      component: () => import('../components/DemoPill.svelte'),
-    },
-    {
-      name: Route.Tabs,
-      path: Path.Tabs,
-      component: () => import('../components/DemoTabs.svelte'),
-    },
-    {
-      name: Route.TabsPanels,
-      path: Path.TabsPanels,
-      component: () => import('../components/DemoTabsPanels.svelte'),
-    },
-    {
-      name: Route.Dividers,
-      path: Path.Dividers,
-      component: () => import('../components/DemoDividers.svelte'),
-    },
-    {
-      name: Route.Text,
-      path: Path.Text,
-      component: () => import('../components/DemoText.svelte'),
-    },
-    {
-      name: Route.Cards,
-      path: Path.Cards,
-      component: () => import('../components/DemoCards.svelte'),
-    },
-    {
-      name: Route.Skeleton,
-      path: Path.Skeleton,
-      component: () => import('../components/DemoSkeleton.svelte'),
-    },
-    {
-      name: Route.Inputs,
-      path: Path.Inputs,
-      component: () => import('../components/DemoInputs.svelte'),
-    },
-    {
-      name: Route.Lists,
-      path: Path.Lists,
-      component: () => import('../components/DemoLists.svelte'),
-    },
-    {
-      name: Route.Progress,
-      path: Path.Progress,
-      component: () => import('../components/DemoProgress.svelte'),
-    },
-    {
-      name: Route.Collapse,
-      path: Path.Collapse,
-      component: () => import('../components/DemoCollapse.svelte'),
-    },
-    {
-      name: Route.Stepper,
-      path: Path.Stepper,
-      component: () => import('../components/DemoStepper.svelte'),
-    },
-    {
-      name: Route.Cursor,
-      path: Path.Cursor,
-      component: () => import('../components/DemoCursor.svelte'),
-    },
-    {
-      name: Route.Tooltips,
-      path: Path.Tooltips,
-      component: () => import('../components/DemoTooltips.svelte'),
-    },
-    {
-      name: Route.Dialog,
-      path: Path.Dialog,
-      component: () => import('../components/DemoDialog.svelte'),
-    },
-    {
-      name: Route.Any,
-      path: Path.Any,
-      redirect: {
-        name: Route.Buttons,
-      },
-    },
-  ] as const,
-};
+  routes: Object.entries(Route).map(([k, v]) => {
+    if (v === Route.Any) return { name: v, path: PathMap[v], redirect: { name: Route.Buttons } };
+    return { name: v, path: PathMap[v], component: () => import(`../components/Demo${k}.svelte`) };
+  }),
+} as const;
