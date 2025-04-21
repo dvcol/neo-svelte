da77cd7a082bface934a9135bea4088493af49d1
Dinh-Van Colomban <van.colomban@gmail.com>
2025-04-21 17:08:06 +0200

    fix(portal): adds missing export

--- src/lib/floating/portal/index.ts
+++ src/lib/floating/portal/index.ts
@@ -1,3 +1,5 @@
 export { default as NeoPortal } from './NeoPortal.svelte';
+export { default as NeoPortalContainer } from './NeoPortalContainer.svelte';
 
+export type * from './neo-portail-container.model.js';
 export type * from './neo-portal.model.js';
