1376474884e3ffd473624b8b9d6b730a339716fb
Dinh-Van Colomban <van.colomban@gmail.com>
2025-08-17 16:17:30 +0200

    fix(floating): export notifications

--- src/lib/floating/index.ts
+++ src/lib/floating/index.ts
@@ -1,5 +1,6 @@
 export * from './dialog/index.js';
 export * from './drawer/index.js';
 export * from './menu/index.js';
+export * from './notification/index.js';
 export * from './portal/index.js';
 export * from './tooltips/index.js';
---
+++ src/lib/floating/notification/index.ts
@@ -0,0 +1,11 @@
+export * from './neo-notification-stack.model.js';
+export * from './neo-notification.model.js';
+export * from './neo-simple-notification.model.js';
+
+export type * from './neo-notification-provider.model.js';
+
+export { useNotificationService } from './neo-notification-provider.model.js';
+
+export { default as NeoNotificationProvider } from './NeoNotificationProvider.svelte';
+export { default as NeoNotificationStack } from './NeoNotificationStack.svelte';
+export { default as NeoSimpleNotification } from './NeoSimpleNotification.svelte';
